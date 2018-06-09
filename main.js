4b7eebb9e9f0ec38668f35d0cb0598917820ec3c
unknown <ko19950314@gmail.com>
2018-06-09 17:39:46 +0800

    added command line interaction

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const decodeMHG = require('./decodeMHG');
 const rp = require('request-promise');
 const { JSDOM } = require('jsdom');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 Number.prototype.pad = function(size) {
     var s = String(this);
@@ -16,49 +17,27 @@ class Sanki {
         this.baseFolder = 'manga';
     }
 
-    async init(){
-        logger.info('init phantom');
-        this.instance = await phantom.create();
-        this.page = await this.instance.createPage();
+    getChapterUrls(mangaUrl){
+        return rp(mangaUrl).then(async function(html){
+            const { document } = (new JSDOM(html)).window;
+            const index = [];
+            document.querySelectorAll(".chapter-list a").forEach(function(chapter){
+                index.push({ name: chapter.title, path: chapter.href })
+            })
+            return index
+       })
     }
 
-    async getChapterUrls(url){
-        
-    }
-
-    async _parseChapter(html){
-        const { document } = (new JSDOM(html)).window;
-        const parsedChapter = {
-            data: {
-                name: document.querySelector("body > div.w980.title > div:nth-child(2) > h1 > a").innerHTML,
-                totalPages: document.querySelector("#pageSelect > option:last-child").value,
-                chapter: document.querySelector("body > div.w980.title > div:nth-child(2) > h2").innerHTML, 
-            }, 
-            dev: {
-                imgSrc: document.querySelector("#mangaFile").src, 
-            },
-            progress: {
-                downloadedPages: 0,
-                missingPages: [],
-            },
-            document: document,
-        }
-        logger.debug(parsedChapter);
-        return parsedChapter 
-    }
-    async test(){
-        let html = await rp('https://tw.manhuagui.com/comic/5060/249045.html');
-        let chapter = this.parseChapter(html);     
-    }
-    async parseChapter(html){
-        let encodeParams = html.match(/\('1l.1m\((.*)\)\.2C\(\);',(.*),(.*),(.*),(.*),(.*)\)\) \<\/script\>/);
+    async parseChapter(chapterUrl){
+        const html = await rp(chapterUrl).catch((err) => logger.error(err));
+        let encodeParams = html.match(/\('.*\..*\((.*)\)\..*\(\);',(.*),(.*),(.*),(.*),(.*)\)\) \<\/script\>/);
         encodeParams = encodeParams.slice(1)
         encodeParams[1] = parseInt(encodeParams[1])
         encodeParams[2] = parseInt(encodeParams[2])
         encodeParams[3] = eval(encodeParams[3])
         encodeParams[4] = parseInt(encodeParams[4])
         encodeParams[5] = {}
-        let raw = JSON.parse(decodeMHG(encodeParams[0], encodeParams[1], encodeParams[2], encodeParams[3], encodeParams[4], encodeParams[5]))
+        let raw = JSON.parse(decodeMHG(...encodeParams))
         let json = {
             name: raw.bname,
             totalPages: raw.len,
@@ -77,53 +56,91 @@ class Sanki {
         }
         return json 
     }
-    downloadImage(chapter){
+
+    prepareDownloadTasks(chapter){
         let imgs = chapter.imgs
         let baseUrl = 'https://i.hamreus.com'
-        let baseFolder = this.baseFolder
-        let promises = [];
-        for(let i = 0; i < imgs.files.length; i++){
-            let path = `${baseUrl}${imgs.path}${imgs.files[i]}?cid=${imgs.cid}&md5=${imgs.md5}`
-            promises.push(rp({
-                url: path,
-                encoding: null, // binary mode
-                headers: {
-                    'Referer': 'https://tw.manhuagui.com', // fake referer
-                }
-            }).then(function(content){
-                if(!fs.existsSync(`${baseFolder}/${chapter.name}`)){
-                   fs.mkdirSync(`${baseFolder}/${chapter.name}`) 
-                }
-                if(!fs.existsSync(`${baseFolder}/${chapter.name}/${chapter.chapter}`)){
-                   fs.mkdirSync(`${baseFolder}/${chapter.name}/${chapter.chapter}`) 
-                }
-                fs.writeFile(`${baseFolder}/${chapter.name}/${chapter.chapter}/${i}.jpg`, content, function(err) {
-                    if(err){
+        let mangaFolder = `${this.baseFolder}/${chapter.name}`
+        let chapterFolder = `${mangaFolder}/${chapter.chapter}`
+        let tasks = [];
+        for(let i = 0; i < chapter.imgs.files.length; i++){
+            tasks.push(() => {
+                let path = `${baseUrl}${imgs.path}${imgs.files[i]}?cid=${imgs.cid}&md5=${imgs.md5}`
+                return new Promise((resolve, reject) => {
+                    rp({
+                        url: path,
+                        encoding: null, // binary mode
+                        headers: {
+                            'Referer': 'https://tw.manhuagui.com', // fake referer
+                        }
+                    })
+                    .then(function(content){
+                        if(!fs.existsSync(mangaFolder)){
+                            fs.mkdirSync(mangaFolder) 
+                        }
+                        if(!fs.existsSync(chapterFolder)){
+                            fs.mkdirSync(chapterFolder) 
+                        }
+                        fs.writeFile(`${chapterFolder}/${i}.jpg`, content, function(err) {
+                            if(err){
+                                chapter.progress.missingPages += i
+                                logger.error('image IO error', err)
+                            }
+                            chapter.progress.downloadedPages += 1
+                            logger.info(`page ${i} downloaded`)
+                            return resolve(i)
+                        })
+                    })
+                    .catch(function(err){
                         chapter.progress.missingPages += i
-                        logger.error('image IO error', err)
-                    }
-                    chapter.progress.downloadedPages += 1
-                    logger.info(`${i} downloaded`)
+                        logger.error('can not fetch the image', err)
+                        reject()
+                    })
                 })
-            }).catch(function(err){
-                chapter.progress.missingPages += i
-                logger.error('can not fetch the image', err)
-            }))
+            })
         }
-        Promise.all(promises).then(function(result){
-            console.log("done");
+        return tasks;
+    }
+    startDownloadTasks(tasks){
+        Promise.all(tasks.map(task => task())).then(function(result){
+            logger.info("finished")
         })
     }
     async downloadChapter(chapterUrl){
-        const html = await rp('https://tw.manhuagui.com/comic/5060/249045.html');
-        logger.info('html fetched')
-        let chapter = await this.parseChapter(html);
-        logger.info('chapter info parsed')
-        this.downloadImage(chapter)
+        logger.info('Parsing chapter HTML')
+        let chapterMeta = await this.parseChapter(chapterUrl);
+        logger.info('Preparing download tasks')
+        let tasks = await this.prepareDownloadTasks(chapterMeta)
+        logger.info('Start downloading')
+        this.startDownloadTasks(tasks)
     }
 };
 
 (async function(){
     const sanki = new Sanki();
-    await sanki.downloadChapter()
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'mangaPath',
+            message: "What is the path of the manga"
+        }
+    ]).then(async function(answers){
+        const chapters = await sanki.getChapterUrls(answers.mangaPath);
+        inquirer.prompt([{
+            type: 'checkbox',
+            name: 'chapters',
+            message: 'Which chapter(s) you want to download?',
+            choices: chapters.map(function(chapter){
+                return {
+                    name: chapter.name,
+                    value: 'https://tw.manhuagui.com' + chapter.path //fix
+                }
+            })
+
+        }]).then(function(answers){
+            answers.chapters.map(function(selectedChapterUrl){
+                sanki.downloadChapter(selectedChapterUrl)
+            })
+        })
+    })
 })()
